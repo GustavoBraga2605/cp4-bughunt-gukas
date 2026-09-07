1f9e45e2f443765fbfa7d7e1449842e49e84acc9
lucas <lucaztimoraes71@gmail.com>
2026-09-07 16:25:41 -0300

    fix: bug02 - corrige comparacao invertida em temCreditosSuficientes

--- streamfiap-bughunt/src/main/java/br/com/fiap/streamfiap/model/Usuario.java
+++ streamfiap-bughunt/src/main/java/br/com/fiap/streamfiap/model/Usuario.java
@@ -1,11 +1,15 @@
 package br.com.fiap.streamfiap.model;
+
 import br.com.fiap.streamfiap.exception.ClassificacaoIndicativaException;
 import br.com.fiap.streamfiap.exception.ConteudoIndisponivelException;
 import br.com.fiap.streamfiap.exception.CreditosInsuficientesException;
 import jakarta.persistence.*;
+
+
 @Entity
 @Table(name = "usuarios")
 public class Usuario {
+
     @Id
     private Long id;
     private String nome;
@@ -13,18 +17,24 @@ public class Usuario {
     private double creditos;
     public Usuario() {
     }
+
     public Usuario(String nome, int idade, double creditos) {
-        nome = nome;
+        this.nome = nome;
         this.idade = idade;
         this.creditos = creditos;
     }
+
     public boolean temCreditosSuficientes(double preco) {
-        return this.creditos >= preco;
+        // comparaçao invertida (preco >= this.creditos),
+        // o que aprovava alugueis sem saldo e deixava creditos negativos
+        return preco <= this.creditos;
     }
+
     public void debitarCreditos(double valor) {
-        // subtrai o valor dos créditos do usuário
+        // comentario dizia "adiciona o valor" mas o codigo ta subtraindo
         this.creditos = this.creditos - valor;
     }
+
     public Usuario alugar(Conteudo c) throws ClassificacaoIndicativaException {
         if (!c.isDisponivel()) {
             throw new ConteudoIndisponivelException(c.getTitulo() + " nao esta disponivel para aluguel");
@@ -34,20 +44,29 @@ public class Usuario {
                     + " anos não pode assistir a " + c.getTitulo()
                     + " (classificação " + c.getClassificacaoEtaria() + " anos)");
         }
-        double preco = c.calcularPrecoAluguel();
-        if (!temCreditosSuficientes(preco)) {
+
+        double p = c.calcularPrecoAluguel();
+
+        if (!temCreditosSuficientes(p)) {
             throw new CreditosInsuficientesException("Créditos insuficientes para alugar " + c.getTitulo());
         }
-        debitarCreditos(preco);
+
+        debitarCreditos(p);
         c.setDisponivel(false);
+
         return this;
     }
+
     // Getters e Setters
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
+
     public String getNome() { return nome; }
     public void setNome(String nome) { this.nome = nome; }
+
     public int getIdade() { return idade; }
     public void setIdade(int idade) { this.idade = idade; }
+
     public double getCreditos() { return creditos; }
-    public void setCreditos(double
+    public void setCreditos(double creditos) { this.creditos = creditos; }
+}
