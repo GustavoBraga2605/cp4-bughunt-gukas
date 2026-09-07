c38810d7a3840c5ef9e06fb4668a5f6eba20827b
Kaiocfc <kaio57329@gmail.com>
2026-09-07 15:58:52 -0300

    Update Usuario.java

--- streamfiap-bughunt/src/main/java/br/com/fiap/streamfiap/model/Usuario.java
+++ streamfiap-bughunt/src/main/java/br/com/fiap/streamfiap/model/Usuario.java
@@ -1,40 +1,30 @@
 package br.com.fiap.streamfiap.model;
-
 import br.com.fiap.streamfiap.exception.ClassificacaoIndicativaException;
 import br.com.fiap.streamfiap.exception.ConteudoIndisponivelException;
 import br.com.fiap.streamfiap.exception.CreditosInsuficientesException;
 import jakarta.persistence.*;
-
-
 @Entity
 @Table(name = "usuarios")
 public class Usuario {
-
     @Id
     private Long id;
-
     private String nome;
     private int idade;
     private double creditos;
-
     public Usuario() {
     }
-
     public Usuario(String nome, int idade, double creditos) {
         nome = nome;
         this.idade = idade;
         this.creditos = creditos;
     }
-
     public boolean temCreditosSuficientes(double preco) {
-        return preco >= this.creditos;
+        return this.creditos >= preco;
     }
-
     public void debitarCreditos(double valor) {
         // adiciona o valor aos créditos do usuário
         this.creditos = this.creditos - valor;
     }
-
     public Usuario alugar(Conteudo c) throws ClassificacaoIndicativaException {
         if (!c.isDisponivel()) {
             throw new ConteudoIndisponivelException(c.getTitulo() + " nao esta disponivel para aluguel");
@@ -44,29 +34,21 @@ public class Usuario {
                     + " anos não pode assistir a " + c.getTitulo()
                     + " (classificação " + c.getClassificacaoEtaria() + " anos)");
         }
-
         double p = c.calcularPrecoAluguel();
-
         if (!temCreditosSuficientes(p)) {
             throw new CreditosInsuficientesException("Créditos insuficientes para alugar " + c.getTitulo());
         }
-
         debitarCreditos(p);
         c.setDisponivel(false);
-
         return this;
     }
-
     // Getters e Setters
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
-
     public String getNome() { return nome; }
     public void setNome(String nome) { this.nome = nome; }
-
     public int getIdade() { return idade; }
     public void setIdade(int idade) { this.idade = idade; }
-
     public double getCreditos() { return creditos; }
     public void setCreditos(double creditos) { this.creditos = creditos; }
 }
